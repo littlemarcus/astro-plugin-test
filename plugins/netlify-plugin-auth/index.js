6f93b4cac11681d7c6440e85a40a09e642d3c59a
Marcus Little <mcolelittle@gmail.com>
2025-02-11 14:17:36 -0500

    switch to es modules

--- plugins/netlify-plugin-auth/index.js
+++ plugins/netlify-plugin-auth/index.js
@@ -1,15 +1,13 @@
-module.exports = {
-    onPreBuild({ netlifyConfig, utils }) {
-        const basicAuth = 'test:344157test';
+export const onPreBuild = function ({ netlifyConfig, utils }) {
+    const basicAuth = 'test:344157test';
 
-        if (basicAuth === undefined) {
-            utils.build.failBuild('      BASIC_AUTH is not defined!');
-        }
-
-        console.log('      Setting basic auth:', basicAuth);
-        netlifyConfig.headers.push({
-            for: '/*',
-            values: { 'Basic-Auth': basicAuth }
-        });
+    if (basicAuth === undefined) {
+        utils.build.failBuild('      BASIC_AUTH is not defined!');
     }
+
+    console.log('      Setting basic auth:', basicAuth);
+    netlifyConfig.headers.push({
+        for: '/*',
+        values: { 'Basic-Auth': basicAuth }
+    });
 };
