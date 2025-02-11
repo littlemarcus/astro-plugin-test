f8e3f38dc248d557b580dcc424ebede153eca2f0
Marcus Little <mcolelittle@gmail.com>
2025-02-11 14:11:07 -0500

    add auth plugin

---
+++ plugins/netlify-plugin-auth/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    onPreBuild({ netlifyConfig, utils }) {
+        const basicAuth = 'test:344157test';
+
+        if (basicAuth === undefined) {
+            utils.build.failBuild('      BASIC_AUTH is not defined!');
+        }
+
+        console.log('      Setting basic auth:', basicAuth);
+        netlifyConfig.headers.push({
+            for: '/*',
+            values: { 'Basic-Auth': basicAuth }
+        });
+    }
+};
